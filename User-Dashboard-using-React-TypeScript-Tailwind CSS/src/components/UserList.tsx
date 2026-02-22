aa60485324100231af3f73be954ba7951976cb9a
Siam Ahmed <siamahmed.only@gmail.com>
2026-02-22 16:13:37 +0600

    22-02-26

--- User-Dashboard-using-React-TypeScript-Tailwind CSS/src/components/UserList.tsx
+++ User-Dashboard-using-React-TypeScript-Tailwind CSS/src/components/UserList.tsx
@@ -1,48 +1,102 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 import type { User } from "../types/user";
 import { getUsers } from "../services/user";
 
-
-
 const UserList = () => {
+  const [users, setUsers] = useState<User[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
 
-    const [users, setUsers] = useState<User[]>([]);
-    const [loading, setLoading] = useState<boolean>(true);
-    const [error, setError] = useState<string | null>(null);
+  useEffect(() => {
+    fetchUser();
+  }, []);
 
-    useEffect(() => {
-        fetchUser()
-    }, [])
-
-    const fetchUser = async() => {
-        try {
-            setLoading(true);
-            const data = await getUsers();
-            setUsers(data);
-            setError(null)
-        } catch  {
-            setError("Faild to fetch users")
-        } finally {
-            setLoading(false)
-        }
+  const fetchUser = async () => {
+    try {
+      setLoading(true);
+      const data = await getUsers();
+      setUsers(data);
+      setError(null);
+    } catch {
+      setError("Faild to fetch users");
+    } finally {
+      setLoading(false);
     }
+  };
 
-    if(loading) return (
-        <div className="flex items-center justify-center h-64">
-            <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-gray-900"></div>
-        </div>
-    )
+  if (loading)
+    return (
+      <div className="flex items-center justify-center h-64">
+        <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-gray-900"></div>
+      </div>
+    );
 
-    if(error) return (
-        <div className="bg-red-100 border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
-            <strong className="font-bold">Error: </strong>
-            <span className="block sm:inline">{error}</span>
-        </div>
-    )
+  if (error)
+    return (
+      <div
+        className="bg-red-100 border-red-400 text-red-700 px-4 py-3 rounded relative"
+        role="alert"
+      >
+        <strong className="font-bold">Error: </strong>
+        <span className="block sm:inline">{error}</span>
+      </div>
+    );
 
   return (
-    <div>UserList</div>
-  )
-}
+    <div>
 
-export default UserList
+        {/* Add New User Form */}
+        <div className="bg-white shadow rounded-lg p-6">
+            <h3 className="text-lg font-medium text-gray-900 mb-4 text-left">Add New User</h3>
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+                <input type="text" 
+                placeholder="Name"
+                className="block w-full p-2 rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"/>
+                <input type="text" 
+                placeholder="Username"
+                className="block w-full p-2 rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"/>
+                <input type="email" 
+                placeholder="email"
+                className="block w-full p-2 rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"/>
+            </div>
+
+            <div className="mt-4 text-left">
+                <button className="inline-flex text-left items-center justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 cursor-pointer">Add User</button>
+            </div>
+        </div>
+
+      {/* user list */}
+      <div className="bg-white shadow rounded-lg overflow-hidden">
+        <div className="px-4 py-5 sm:px-6">
+          <h2 className="text-lg font-medium text-gray-900 text-left">Users</h2>
+        </div>
+
+        <ul className="divide-y divide-gray-200 text-left">
+          {users.map((user) => (
+            <li key={user.id} className="px-4 py-4 sm:px-6">
+              <div className="flex items-center">
+                <div className="flex-1 min-w-0">
+                  <p className="text-sm font-medium text-indigo-600 truncate">
+                    {user.name}
+                  </p>
+                  <p className="text-sm text-gray-500">{user.email}</p>
+                </div>
+
+                <div className="flex space-x-2">
+                  <button className="inline-flex items-center px-3 py-1.5 border border-transparent text-xs font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 cursor-pointer duration-300">
+                    Update
+                  </button>
+                  <button className="inline-flex items-center px-3 py-1.5 border border-transparent text-xs font-medium rounded-md text-red-700 bg-red-100 hover:bg-red-200 cursor-pointer duration-300">
+                    Delete
+                  </button>
+                </div>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default UserList;
--- User-Dashboard-using-React-TypeScript-Tailwind CSS/src/services/user.ts
+++ User-Dashboard-using-React-TypeScript-Tailwind CSS/src/services/user.ts
@@ -11,3 +11,19 @@ export const getUsers = async(): Promise<User[]> => {
 
     return response.json()
 }
+
+export const createUser = async(user: Omit<User, "id">) : Promise<User[]> => {
+    const response = await fetch(API_URL, {
+        method: "POST",
+        headers: {
+            "Content-Type" : "application/json"
+        },
+        body: JSON.stringify(user)
+    })
+
+    if(!response.ok) {
+        throw new Error("Faild to create new user")
+    }
+
+    return response.json()
+}
